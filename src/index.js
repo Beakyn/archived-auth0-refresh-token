b08f465b6348f2fe14ff7c0148fccbc1fddd3714
Juan Pujol <contact@juanpujol.com>
2018-08-28 15:36:48 -0300

    refactor(index): improve code style

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const query = require("micro-query");
-const cors = require("micro-cors")();
-const { send } = require("micro");
-const axios = require("axios");
+const query = require('micro-query');
+const cors = require('micro-cors')();
+const { send } = require('micro');
+const axios = require('axios');
 
 const getRefreshToken = async ({
   domain,
@@ -21,17 +21,17 @@ const handler = async (req, res) => {
   const q = query(req);
 
   try {
-    if (req.method === "GET") {
+    if (req.method === 'GET') {
       const { data } = await getRefreshToken(q);
 
       return send(res, 200, data);
     }
 
-    return send(res, 405, "Invalid Method");
+    return send(res, 405, 'Invalid Method');
   } catch (error) {
     const errorResp = error.response;
 
-    console.error("ERROR:", error.response);
+    console.error('ERROR:', error.response);
 
     return send(res, errorResp.status, { ...errorResp.data });
   }
