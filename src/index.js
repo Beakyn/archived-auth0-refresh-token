35d981e19a596a825a7715bbfdecf6d45d5aef7c
Nelson Eldoro <nelsoneldoro@gmail.com>
2018-08-28 15:22:21 -0300

    fix(app): destruct data var

--- src/index.js
+++ src/index.js
@@ -1,34 +1,40 @@
-const query = require('micro-query');
-const cors = require('micro-cors')();
-const {send} = require('micro');
-const axios = require('axios');
+const query = require("micro-query");
+const cors = require("micro-cors")();
+const { send } = require("micro");
+const axios = require("axios");
 
-const getRefreshToken = async ({domain, grant_type, client_id, code, redirect_uri}) =>
+const getRefreshToken = async ({
+  domain,
+  grant_type,
+  client_id,
+  code,
+  redirect_uri
+}) =>
   axios.post(`https://${domain}/oauth/token`, {
     grant_type,
     client_id,
     code,
-    redirect_uri,
-  })
+    redirect_uri
+  });
 
 const handler = async (req, res) => {
   const q = query(req);
 
   try {
-    if (req.method === 'GET') {
-      const data = await getRefreshToken(q)
+    if (req.method === "GET") {
+      const { data } = await getRefreshToken(q);
 
       return send(res, 200, data);
     }
 
-    return send(res, 405, 'Invalid Method');
+    return send(res, 405, "Invalid Method");
   } catch (error) {
     const errorResp = error.response;
 
-    console.error('ERROR:', error.response);
+    console.error("ERROR:", error.response);
 
-    return send(res, errorResp.status, {...errorResp.data});
+    return send(res, errorResp.status, { ...errorResp.data });
   }
-}
+};
 
-module.exports = cors(handler)
+module.exports = cors(handler);
